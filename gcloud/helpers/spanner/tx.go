7687c5e1fe98508c55ae7efb9d7dbd645242623a
Dany Sousa <dany.sousa@musicworldmedia.com>
2023-07-17 16:14:50 +0200

    [GCloud Spanner] Init module with basic functions and models that help developer to keep a clean code

---
+++ gcloud/helpers/spanner/tx.go
@@ -0,0 +1,19 @@
+package spanner
+
+import (
+	"context"
+
+	"cloud.google.com/go/spanner"
+	"cloud.google.com/go/spanner/apiv1/spannerpb"
+)
+
+// Tx is a common interface for spanner read only and read/write transactions that only read
+type Tx interface {
+	Read(ctx context.Context, table string, keys spanner.KeySet, columns []string) *spanner.RowIterator
+	ReadUsingIndex(ctx context.Context, table, index string, keys spanner.KeySet, columns []string) (ri *spanner.RowIterator)
+	ReadWithOptions(ctx context.Context, table string, keys spanner.KeySet, columns []string, opts *spanner.ReadOptions) (ri *spanner.RowIterator)
+	ReadRow(ctx context.Context, table string, key spanner.Key, columns []string) (*spanner.Row, error)
+	Query(ctx context.Context, statement spanner.Statement) *spanner.RowIterator
+	QueryWithStats(ctx context.Context, statement spanner.Statement) *spanner.RowIterator
+	AnalyzeQuery(ctx context.Context, statement spanner.Statement) (*spannerpb.QueryPlan, error)
+}
