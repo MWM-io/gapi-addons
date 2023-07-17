fe1a99a18a068ae222394ea10e4f2d5fc59f1871
Dany Sousa <dany.sousa@musicworldmedia.com>
2023-07-17 17:07:40 +0200

    [GCloud Spanner] Fix comment typo & Add IterToStruct func

--- gcloud/helpers/spanner/iter.go
+++ gcloud/helpers/spanner/iter.go
@@ -5,7 +5,7 @@ import (
 )
 
 // GetOneWithIterator is a helper function to get one row from a row iterator
-// You can use it to simply your code when you know there is only one row in the iterator.
+// You can use it to simplify your code when you know there is only one row in the iterator.
 func GetOneWithIterator(iter *spanner.RowIterator) (row *spanner.Row, exist bool, err error) {
 	err = iter.Do(func(r *spanner.Row) error {
 		exist = true
@@ -17,7 +17,7 @@ func GetOneWithIterator(iter *spanner.RowIterator) (row *spanner.Row, exist bool
 }
 
 // IterToStructs is a helper function to process a row iterator and convert each row to a struct
-// This function will an array of structs and/or the spanner error if one occurs.
+// This function will return an array of structs and/or the spanner error if one occurs.
 func IterToStructs[T any](iter *spanner.RowIterator) ([]T, error) {
 	var results []T
 
@@ -32,4 +32,20 @@ func IterToStructs[T any](iter *spanner.RowIterator) ([]T, error) {
 	})
 
 	return results, err
+}
+
+// IterToStruct is a helper function to process and convert one row from a row iterator
+// You can use it to simplify your code when you know there is only one row in the iterator.
+func IterToStruct[T any](iter *spanner.RowIterator) (result T, exist bool, err error) {
+	results, errIter := IterToStructs[T](iter)
+
+	if err != nil {
+		return result, false, errIter
+	}
+
+	if exist = len(results) > 0; exist {
+		result = results[0]
+	}
+
+	return
 }
